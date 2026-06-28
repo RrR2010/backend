6932a0ec6bd1ea6c1473d3aa2478ab3d65145fbd
Richard De Souza <richard@viversorvete.com>
2026-06-28 13:00:39 -0300

    fix(ep-005): correções T-030/031 pós-RV
    - Low: AlreadyExistsError removidos

---
+++ src/modules/ingredients/technical-source-type-pl.errors.ts
@@ -0,0 +1,13 @@
+import { HttpException, HttpStatus } from '@nestjs/common'
+
+export class TechnicalSourceType_PLNotFoundError extends HttpException {
+  constructor() {
+    super(
+      {
+        message: 'Resource not found or access denied',
+        code: 'NOT_FOUND'
+      },
+      HttpStatus.NOT_FOUND
+    )
+  }
+}
